f8ea2c2ee0111b48f4f5facfc1f8a5e655f29675
rayyu <rayyu03@163.com>
2016-09-07 22:13:35 +0800

    增加修改EMAIL功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, login_required, logout_user, current_user
 from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm, ChangePasswordForm, \
-                    PasswordResetForm, PasswordResetRequestForm
+                    PasswordResetForm, PasswordResetRequestForm, ChangeEmailForm
 from .. import db
 from ..email import send_email
 
@@ -122,3 +122,29 @@ def password_reset(token):
         else:
             return redirect(url_for('main.home'))
     return render_template('auth/reset_password.html', form=form)
+
+@auth.route('/change-email', methods=['GET', 'POST'])
+@login_required
+def change_email_request():
+    form = ChangeEmailForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.password.data):
+            new_email = form.email.data
+            token = current_user.generate_email_change_token(new_email)
+            send_email(new_email,'Confirm your email address',
+                    'auth/email/change_email',user=current_user,token=token)
+            flash('An email with instructions to confirm your new email address'
+                    'has benn sent to you')
+            return redirect(url_for('main.home'))
+        else:
+            flash('Invaild email or password')
+    return  render_template('auth/change_email.html',form=form)
+
+@auth.route('/change-email/<token>')
+@login_required
+def change_email(token):
+    if current_user.change_email(token):
+        flash('Your email address has been updated.')
+    else:
+        flash('Invaild request.')
+    return redirect(url_for('main.home'))
