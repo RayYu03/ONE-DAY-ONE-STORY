76abdbb82dd6f272378fb8cbe22fab68e63cb957
rayyu <rayyu03@163.com>
2016-09-08 13:04:10 +0800

    分配用户权限

---
+++ app/decorators.py
@@ -0,0 +1,16 @@
+from functools import wraps
+from flask import abort
+from flask_login import current_user
+
+def permissions_required(permission):
+    def decorator(f):
+        @wraps
+        def decorated_function(*args,**kwargs):
+            if not current_user.can(permission):
+                abort(403)
+            return f(*args,**kwargs)
+        return decorated_function
+    return decorator
+
+def admin_required(f):
+    return permissions_required(Permission.ADMINISTER)(f)
--- app/models.py
+++ app/models.py
@@ -1,19 +1,51 @@
+# -*- coding:utf-8 -*-
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask_login import UserMixin
+from flask_login import UserMixin,AnonymousUserMixin
 from flask import current_app
 from . import login_manager
 from . import db
 
+#增加角色的权限记录(default,permissions)
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean,default=False,index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    #在数据库中创建角色,只有管理员的default为False
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User':(Permission.FOLLOW |
+                    Permission.COMMENT |
+                    Permission.WRITE_ARTICLES, True),
+            'Moderator':(Permission.FOLLOW |
+                        Permission.COMMENT |
+                        Permission.WRITE_ARTICLES|
+                        Permission.MODERATE_COMMENTS, True),
+            'Administrator':(0xff,False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -23,6 +55,25 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+
+    #赋予用户角色
+    def __init__(self,**kwargs):
+        super(User,self).__init__(**kwargs)
+
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+    #检查用户是否有指定的权限
+    def can(self, permissions):
+        return self.role is not None and \
+            (self.role.permissions & permissions) == permissions
+
+    #检查用户是否有管理员权限
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
 
     @property
     def password(self):
@@ -90,6 +141,14 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
 
 @login_manager.user_loader
 def load_user(user_id):
